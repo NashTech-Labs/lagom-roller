f3a306d94ce70c160782aa7f75d121ff5fcc77f0
Bhavya <bhavya@knoldus.com>
2017-03-03 13:46:46 +0530

    Fixed the problem with the CompletionStage

--- hello-impl/src/main/java/edu/knoldus/hello/impl/DummyDBImpl.java
+++ hello-impl/src/main/java/edu/knoldus/hello/impl/DummyDBImpl.java
@@ -4,6 +4,7 @@ import akka.Done;
 import com.lightbend.lagom.javadsl.persistence.AggregateEventTag;
 import com.lightbend.lagom.javadsl.persistence.Offset;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 /**
@@ -13,18 +14,18 @@ public class DummyDBImpl implements MyDatabase {
     @Override
     public CompletionStage<Done> createTables() {
         System.out.println("Create Tables called");
-        return (CompletionStage<Done>) Done.getInstance();
+        return CompletableFuture.supplyAsync(() -> {return Done.getInstance();});
     }
 
     @Override
     public CompletionStage<Offset> loadOffset(AggregateEventTag<HelloEvent> tag) {
         System.out.println("Load offset called");
-        return null;
+        return CompletableFuture.supplyAsync(() -> {return Offset.NONE;});
     }
 
     @Override
     public CompletionStage<Done> handleEvent(HelloEvent event, Offset offset) {
         System.out.println("Handle event called");
-        return null;
+        return CompletableFuture.supplyAsync(() -> {return Done.getInstance();});
     }
 }
