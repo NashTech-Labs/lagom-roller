6635d7967988ac652855f2107ecee1b45c3bd1e3
Vikas Hazrati <vikas@knoldus.com>
2017-03-03 05:59:32 -0500

    Read side persistence example

--- hello-impl/src/main/java/edu/knoldus/hello/impl/DummyDBImpl.java
+++ hello-impl/src/main/java/edu/knoldus/hello/impl/DummyDBImpl.java
@@ -4,27 +4,25 @@ import akka.Done;
 import com.lightbend.lagom.javadsl.persistence.AggregateEventTag;
 import com.lightbend.lagom.javadsl.persistence.Offset;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
-/**
- * Created by vikas on 03/03/17.
- */
 public class DummyDBImpl implements MyDatabase {
     @Override
     public CompletionStage<Done> createTables() {
         System.out.println("Create Tables called");
-        return (CompletionStage<Done>) Done.getInstance();
+        return CompletableFuture.completedFuture(Done.getInstance());
     }
 
     @Override
     public CompletionStage<Offset> loadOffset(AggregateEventTag<HelloEvent> tag) {
         System.out.println("Load offset called");
-        return null;
+        return CompletableFuture.completedFuture(Offset.NONE);
     }
 
     @Override
     public CompletionStage<Done> handleEvent(HelloEvent event, Offset offset) {
         System.out.println("Handle event called");
-        return null;
+        return CompletableFuture.completedFuture(Done.getInstance());
     }
 }
